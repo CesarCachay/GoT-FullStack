9149717e21b731d54af0316a2ba0e98b09d36ba5
Cesar Cachay <cesarcachay1996@gmail.com>
2019-11-19 08:36:02 -0500

    Add services from internal API

---
+++ frontend/src/services/charsApi.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+// const CHARS_API_URL =
+//   process.env.REACT_APP_CHARS_API_URL || "http://localhost:5000/api/characters";
+
+const CHARS_API_URL =
+  process.env.REACT_APP_CHARS_API_URL || "http://localhost:5000/api";
+
+export const getAllCharacters = async (limit = 10, offset = 0) => {
+  // const response = await fetch(
+  //   `${CHARS_API_URL}?limit=${limit}&offset=${offset}`
+  // );
+  // const results = await response.json();
+  // console.log(results);
+  // return response;
+
+  return await axios.get(
+    `${CHARS_API_URL}/characters?limit=${limit}&offset=${offset}`
+  );
+};
+
+export const getOneCharacter = async id => {
+  // const response = await fetch(`${CHARS_API_URL}/${id}`);
+  // const result = await response.json();
+  // console.log(result);
+  // return response;
+
+  return await axios.get(`${CHARS_API_URL}/characters/${id}`);
+};
